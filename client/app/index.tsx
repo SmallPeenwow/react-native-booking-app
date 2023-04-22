e2558a107986edc1dd61480b4d7cf87fe80dbf5e
SmallPeenwow <bowdencamryn@gmail.com>
2023-04-22 13:55:33 +0200

    SplashScreen Added And Customized Test

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,6 +1,9 @@
 import { Text, View } from 'react-native';
-import { Stack, useRouter } from 'expo-router';
+import { SplashScreen, Stack, useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+
+// SplashScreen styling is in app.json
 
 const Login = () => {
 	const router = useRouter();
@@ -17,28 +20,40 @@ const Login = () => {
 		router.push('/BookingRequests');
 	};
 
+	const [isReady, setIsReady] = useState(false);
+
+	// Must do better useEffect
+	useEffect(() => {
+		setTimeout(() => {
+			setIsReady(true);
+		}, 3000);
+	}, []);
+
 	return (
-		<View className='h-full bg-gray-400 justify-center items-center'>
-			<Stack.Screen
-				options={{
-					title: null,
-					headerRight: () => (
-						<Text className='uppercase text-lg pr-3'>Login</Text>
-					),
-				}}
-			/>
-			<View className='border-2 w-64 p-8 border-black gap-8'>
-				<Text>First Page User Will See</Text>
-				<Text className='text-xl uppercase' onPress={SendToSignUp}>
-					Sign Up
-				</Text>
-				<Text onPress={SendToUserFrontPage}>User Home Page</Text>
+		<>
+			{!isReady && <SplashScreen />}
+			<View className='h-full bg-gray-400 justify-center items-center'>
+				<Stack.Screen
+					options={{
+						title: null,
+						headerRight: () => (
+							<Text className='uppercase text-lg pr-3'>Login</Text>
+						),
+					}}
+				/>
+				<View className='border-2 w-64 p-8 border-black gap-8'>
+					<Text>First Page User Will See</Text>
+					<Text className='text-xl uppercase' onPress={SendToSignUp}>
+						Sign Up
+					</Text>
+					<Text onPress={SendToUserFrontPage}>User Home Page</Text>
+				</View>
+				<View className='border-purple-300 border-2 p-2'>
+					<Text onPress={SendToBookingRequests}>Login as Admin</Text>
+				</View>
+				<StatusBar style='auto' />
 			</View>
-			<View className='border-purple-300 border-2 p-2'>
-				<Text onPress={SendToBookingRequests}>Login as Admin</Text>
-			</View>
-			<StatusBar style='auto' />
-		</View>
+		</>
 	);
 };
 

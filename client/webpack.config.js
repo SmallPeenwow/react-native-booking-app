e414580977e9dca93326e7856db04d2d168c6eb4
SmallPeenwow <bowdencamryn@gmail.com>
2023-04-30 09:24:39 +0200

    Work?

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,5 +17,17 @@ module.exports = async function (env, argv) {
 		use: ['postcss-loader'],
 	});
 
+	config.resolve.alias['moduleA'] = 'moduleB';
+
+	// Maybe you want to turn off compression in dev mode.
+	if (config.mode === 'development') {
+		config.devServer.compress = false;
+	}
+
+	// Or prevent minimizing the bundle when you build.
+	if (config.mode === 'production') {
+		config.optimization.minimize = false;
+	}
+
 	return config;
 };
